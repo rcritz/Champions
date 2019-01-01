36324da4216c75dfef2cfa30b085262594443df5
Richard Critz <rcritz@gmail.com>
2019-01-01 15:54:44 -0500

    Add details screen

--- Sources/App/Controllers/WebController.swift
+++ Sources/App/Controllers/WebController.swift
@@ -4,6 +4,7 @@ import Leaf
 struct WebController: RouteCollection {
     func boot(router: Router) throws {
         router.get(use: indexHandler)
+        router.post(ChampionSelect.self, use: selectHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
@@ -11,9 +12,22 @@ struct WebController: RouteCollection {
         let context = IndexContext(title: "Select a champion", champions: champions)
         return try req.view().render("index", context)
     }
+    
+    func selectHandler(_ req: Request, data: ChampionSelect) throws -> Future<View> {
+        let context = ChampionDetails(champion: RiotAPI.championList[data.champion]!)
+        return try req.view().render("details", context)
+    }
 }
 
 struct IndexContext: Encodable {
     let title: String
     let champions: [String]
+}
+
+struct ChampionSelect: Content {
+    let champion: String
+}
+
+struct ChampionDetails: Encodable {
+    let champion: Champion
 }
